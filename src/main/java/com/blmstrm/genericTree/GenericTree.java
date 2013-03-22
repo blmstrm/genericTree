109ced6d96812a522cce3e7dcec233e6c2a1268f
Kalle Blomström <kalle.blomstrom@gmail.com>
2013-03-22 22:16:54 +0100

    Expands GenericTree class with switch cases in constructor and inserValue methods.

--- src/main/java/com/blmstrm/genericTree/GenericTree.java
+++ src/main/java/com/blmstrm/genericTree/GenericTree.java
@@ -1,7 +1,36 @@
 package com.blmstrm.genericTree;
+
 public class GenericTree{
 
-	public GenericTree(){
+  private enum TreeType  { BINARY_TREE }
 
-	}
+  private TreeType type;
+
+  private int numberOfChildren = 0;
+
+  private GenericNode parent;
+
+  public GenericTree(TreeType t){
+
+    this.parent = new GenericNode(null);
+
+    switch(t){
+      case BINARY_TREE:
+	this.numberOfChildren = 2;
+	this.type = t;
+	break;
+      default:
+	//Throw Undefined three type
+	System.out.println("UNDEFINED TREE TYPE");
+    }
+  }
+
+  public void insertValue(INodeValue value){
+
+    switch(this.type){
+      case BINARY_TREE:
+	break;
+      default:
+    }
+  } 
 }
